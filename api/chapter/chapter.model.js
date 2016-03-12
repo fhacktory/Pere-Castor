0e7228447812b91e0b18f649da65f269ca41246e
cédric Rumiano <cedric.rumiano@outlook.com>
2016-03-12 19:22:43 +0000

    Finish chapter

--- api/chapter/chapter.model.js
+++ api/chapter/chapter.model.js
@@ -1,11 +1,16 @@
 var dbUtils = require('../../lib/dynamoUtils');
+var utils = require('../../lib/utils');
+
+var shortid = require('shortid');
 var AWS = require('aws-sdk');
 AWS.config.update({
     region: 'us-west-2'
 });
 var db = new AWS.DynamoDB();
+var s3 = new AWS.S3();
 
 var tablename = 'chapters';
+var s3Bucket = 'perecastor-files';
 
 var User = {
     getFor: function(code, callback) {
@@ -31,26 +36,151 @@ var User = {
             }
         });
     },
-    create: function(token, name, index, callback) {
-         var params = {
+    create: function(code, name, index, image, callback) {
+        if (image) {
+            var imageBuffer = utils.decodeBase64Image(image);
+        }
+        var chapterId = shortid.generate();
+        var params = {
             Item: {
-                pseudo: {
-                    S: pseudo
+                code: {
+                    S: code
                 },
-                password: {
-                    S: password
+                id: {
+                    S: chapterId
                 },
-                email: {
-                    S: email
+                chapterName: {
+                    S: name
                 },
-                mainStory: {
-                    S: mainStory
+                index: {
+                    N: index.toString()
                 }
             },
             TableName: tablename,
             ConditionExpression: 'attribute_not_exists(pseudo)'
         };
-        db.putItem(params, function(err, data) {
+        if (image) {
+            var paramsS3 = {
+                Bucket: s3Bucket,
+                ACL: 'public-read',
+                Key: code + '/backgrounds/' + chapterId + '.' + imageBuffer.type.substring(imageBuffer.type.indexOf('/') + 1),
+                Body: imageBuffer.data
+            };
+            s3.upload(paramsS3, function(err, data) {
+                if (err) {
+                    callback(err);
+                }
+                else {
+                    params.Item.image = {
+                        S: data.Location
+                    };
+                    db.putItem(params, function(err, data) {
+                        if (err) {
+                            callback(err);
+                        }
+                        else {
+                            callback(null);
+                        }
+                    });
+                }
+            });
+        }
+        else {
+            db.putItem(params, function(err, data) {
+                if (err) {
+                    callback(err);
+                }
+                else {
+                    callback(null);
+                }
+            });
+        }
+    },
+    update: function(code, id, name, index, image, callback) {
+        if (image) {
+            var imageBuffer = utils.decodeBase64Image(image);
+        }
+        var params = {
+            Key: {
+                code: {
+                    S: code
+                },
+                id: {
+                    S: id
+                }
+            },
+            TableName: tablename,
+            AttributeUpdates: {}
+        };
+        if (name) {
+            params.AttributeUpdates.chapterName = {
+                Action: 'PUT',
+                Value: {
+                    S: name
+                }
+            };
+        }
+        if (index) {
+            params.AttributeUpdates.index = {
+                Action: 'PUT',
+                Value: {
+                    N: index.toString()
+                }
+            };
+        }
+        if (image) {
+            var paramsS3 = {
+                Bucket: s3Bucket,
+                ACL: 'public-read',
+                Key: code + '/backgrounds/' + id + '.' + imageBuffer.type.substring(imageBuffer.type.indexOf('/') + 1),
+                Body: imageBuffer.data
+            };
+            s3.upload(paramsS3, function(err, data) {
+                if (err) {
+                    callback(err);
+                }
+                else {
+                    params.AttributeUpdates.image = {
+                        Action: 'PUT',
+                        Value: {
+                            S: data.Location
+                        }
+                    };
+                    db.updateItem(params, function(err, data) {
+                        if (err) {
+                            callback(err);
+                        }
+                        else {
+                            callback(null);
+                        }
+                    });
+                }
+            });
+        }
+        else {
+            db.putItem(params, function(err, data) {
+                if (err) {
+                    callback(err);
+                }
+                else {
+                    callback(null);
+                }
+            });
+        }
+    },
+    delete: function(code, id, callback) {
+        var params = {
+            Key: {
+                code: {
+                    S: code
+                },
+                id: {
+                    S: id
+                }
+            },
+            TableName: tablename
+        };
+        db.deleteItem(params, function(err, data) {
             if (err) {
                 callback(err);
             }
@@ -58,15 +188,7 @@ var User = {
                 callback(null);
             }
         });
-    },
-    update: function(token, id, name, index, callback) {
-
-    },
-    delete: function(token, id, callback) {
-
     }
 };
 
 module.exports = User;
-
-User.getFor('toto');
