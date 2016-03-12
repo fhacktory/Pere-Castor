3c0f4aa351b2fb4c98efd672de2a50827ac7d881
roucas76 <roucas76@gmail.com>
2016-03-12 23:25:10 +0000

    script deploiement

--- deploiement/deploy.js
+++ deploiement/deploy.js
@@ -2,21 +2,36 @@ var AWS = require('aws-sdk');
 AWS.config.update({
     region: 'us-west-2'
 });
+var fs = require('fs');
 
-var lambda= new AWS.Lambda();
+var zip;
 
-var params = {
-  Marker: 'STRING_VALUE',
-  MaxItems: 0
-};
+var lambda = new AWS.Lambda();
+
+var params = {};
 lambda.listFunctions(params, function(err, data) {
-	if (err) {
-		console.log(err, err.stack); 
-	}
-	else{
-		for(var i in data){
-			console.log(i);
-		}
-	}     
-});
+    if (err) {
+        console.log(err, err.stack);
+    }
+    else {
+        zip = new Buffer(fs.readFileSync('./deploiement.zip')).toString('base64');
 
+        for (var i in data.Functions) {
+            var fn = data.Functions[i];
+
+
+            params = {
+                FunctionName: fn.FunctionName,
+                S3Bucket: "deploiement",
+                S3Key:"deploiement.zip"
+            };
+            /* Récupérer infos dans fichier config à partir du nom de la fonction */
+
+            lambda.updateFunctionCode(params, function(err, data) {
+                if (err) console.log(err, err.stack); // an error occurred
+                else console.log(data); // successful response
+            });
+            console.log(data.Functions[i]);
+        }
+    }
+});
