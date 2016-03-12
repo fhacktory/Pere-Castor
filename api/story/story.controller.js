16591f15ac80d1ced1d9b1f90f6f07a142a1f026
cédric Rumiano <cedric.rumiano@outlook.com>
2016-03-12 19:36:29 +0000

    Merge stories

---
+++ api/story/story.controller.js
@@ -0,0 +1,65 @@
+var Story = require('./story.model');
+var jwt = require('jsonwebtoken');
+
+function getPseudoFromToken(token) {
+    var decoded;
+    if (token) {
+        decoded = jwt.verify(token, '§%p3r3c4570r§%');
+    }
+    if (decoded && decoded.pseudo) {
+        return decoded.pseudo;
+    }
+    return null;
+}
+
+exports.getUserStories = function(token, callback) {
+    var pseudo = getPseudoFromToken(token);
+    if (pseudo) {
+        Story.list(pseudo, callback);
+    }
+}
+
+
+exports.getStory = function(token, id, callback) {
+    var pseudo = getPseudoFromToken(token);
+    if (pseudo) {
+        Story.get(pseudo, id, callback);
+    }
+}
+
+exports.addStory = function(token, name, isPublic, callback) {
+    var pseudo = getPseudoFromToken(token);
+    if (pseudo && name) {
+        Story.add(pseudo, name, isPublic, checkAdd(callback));
+    }
+}
+
+function checkAdd(callback) {
+    return function(err, data) {
+        if (err) {
+            if (err.code == 'ConditionalCheckFailedException') {
+                callback(null, false);
+            }
+            else {
+                callback(err);
+            }
+        }
+        else {
+            callback(null, data);
+        }
+    }
+}
+
+exports.updateStory = function(token, id, name, isPublic, callback) {
+    var pseudo = getPseudoFromToken(token);
+    if (pseudo && id) {
+        Story.update(pseudo, id, name, isPublic, callback);
+    }
+}
+
+exports.deleteStory = function(token, id, callback) {
+    var pseudo = getPseudoFromToken(token);
+    if (pseudo && id) {
+        Story.delete(pseudo, id, callback);
+    }
+}
--- api/story/story.model.js
+++ api/story/story.model.js
@@ -3,28 +3,136 @@ AWS.config.update({
     region: 'us-west-2'
 });
 var db = new AWS.DynamoDB();
+var shortid = require('shortid');
 
 var Story = {
     list: function(pseudo, callback) {
         var params = {
-            Key: {
-                code: {
+            ExpressionAttributeValues: {
+                ":code": {
                     S: pseudo
                 }
             },
+            KeyConditionExpression: 'code=:code',
             TableName: 'stories'
         };
         db.query(params, function(err, data) {
             if (err) {
                 callback(err);
-            } else {
+            }
+            else {
+                callback(null, data);
+            }
+        });
+    },
+    get: function(pseudo, id, callback) {
+        var params = {
+            Key: {
+                code: {
+                    S: pseudo
+                },
+                id: {
+                    S: id
+                }
+            },
+            TableName: 'stories'
+        };
+        db.getItem(params, function(err, data) {
+            if (err) {
+                callback(err);
+            }
+            else {
+                callback(null, data);
+            }
+        });
+    },
+    add: function(pseudo, name, isPublic, callback) {
+        var params = {
+            Item: {
+                id: {
+                    S: shortid.generate()
+                },
+                code: {
+                    S: pseudo
+                },
+                storyName: {
+                    S: name
+                },
+                public: {
+                    BOOL: isPublic
+                },
+                date: {
+                    N: Date.now().toString()
+                }
+            },
+            TableName: 'stories',
+            ConditionExpression: 'attribute_not_exists(code) and attribute_not_exists(storyName)'
+        };
+        db.putItem(params, function(err, data) {
+            if (err) {
+                callback(err);
+            }
+            else {
+                callback(null, data);
+            }
+        });
+    },
+    update: function(pseudo, id, storyName, isPublic, callback) {
+        var params = {
+            Key: {
+                code: {
+                    S: pseudo
+                },
+                id: {
+                    S: id
+                }
+            },
+            TableName: 'stories',
+            AttributeUpdates: {
+                storyName: {
+                    Action: "PUT",
+                    Value: {
+                        S: storyName
+                    }
+                },
+                public: {
+                    Action: "PUT",
+                    Value: {
+                        BOOL: isPublic
+                    }
+                }
+            }
+        };
+        db.updateItem(params, function(err, data) {
+            if (err) {
+                callback(err);
+            }
+            else {
+                callback(null, data);
+            }
+        });
+    },
+    delete: function(pseudo, id, callback) {
+        var params = {
+            Key: {
+                code: {
+                    S: pseudo
+                },
+                id: {
+                    S: id
+                }
+            },
+            TableName: 'stories'
+        };
+        db.deleteItem(params, function(err, data) {
+            if (err) {
+                callback(err);
+            }
+            else {
                 callback(null, data);
             }
         });
     }
-    get:function(){
-
-    }
 }
 
-module.export = Story;
+module.exports = Story;
