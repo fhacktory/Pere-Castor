027ce9333b0ee91a817ab62ad3a19a23e66ddef8
JustJerem <guillot.jeremie@gmail.com>
2016-03-12 17:52:12 +0100

    Ajout du code généra par Ionic

---
+++ code-app/IonicPereCastor/www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+      
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('chapitreCtrl', function($scope) {
+
+})
+   
+.controller('storyCtrl', function($scope) {
+
+})
+ 
